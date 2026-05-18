e74b849b99e5a9879373196b8953d5f9b81355fa
liuchuancong <chuancong@wangdou-hbean.com>
2026-05-18 09:29:12 +0800

    恢复supabase

---
+++ lib/modules/auth/sign_in_page.dart
@@ -0,0 +1,47 @@
+import 'package:get/get.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pure_live/common/index.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:pure_live/modules/auth/auth_controller.dart';
+import 'package:pure_live/modules/auth/components/supa_email_auth.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(i18n('supabase_sign_in'))),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              SupaEmailAuth(
+                redirectTo: kIsWeb ? null : 'purelive://signin',
+                onPasswordResetEmailSent: () {
+                  final AuthController authController = Get.find<AuthController>();
+                  authController.shouldGoReset = true;
+                  ToastUtil.show('请打开邮箱重置密码');
+                },
+                onSignInComplete: (AuthResponse response) {
+                  ToastUtil.show(i18n('supabase_sign_success'));
+                  Get.offAllNamed(RoutePath.kInitial);
+                },
+                onSignUpComplete: (AuthResponse response) {
+                  ToastUtil.show(i18n('supabase_sign_confirm'));
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -8,6 +8,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   flutter_acrylic
   flutter_inappwebview_linux
   flutter_js
+  gtk
   media_kit_libs_linux
   media_kit_video
   screen_retriever_linux
