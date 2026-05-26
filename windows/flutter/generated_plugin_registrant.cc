cfd254d7ba015f0ab8ce8697aaf477aa50f7585a
liuchuancong <chuancong@wangdou-hbean.com>
2026-05-26 15:03:43 +0800

    add audio_service

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,6 +7,7 @@
 #include "generated_plugin_registrant.h"
 
 #include <app_links/app_links_plugin_c_api.h>
+#include <audio_service_win/audio_service_win_plugin_c_api.h>
 #include <battery_plus/battery_plus_windows_plugin.h>
 #include <charset_converter/charset_converter_plugin.h>
 #include <connectivity_plus/connectivity_plus_windows_plugin.h>
@@ -29,6 +30,8 @@
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AppLinksPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AppLinksPluginCApi"));
+  AudioServiceWinPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AudioServiceWinPluginCApi"));
   BatteryPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("BatteryPlusWindowsPlugin"));
   CharsetConverterPluginRegisterWithRegistrar(
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
+  audio_service_win
   battery_plus
   charset_converter
   connectivity_plus
