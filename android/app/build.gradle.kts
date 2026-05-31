f9f34db0ec00b8a74f12e29ffa0739f9ef7fd152
liuchuancong <17792321552@163.com>
2026-05-31 18:34:02 +0800

    chore: 移除 ndk abiFilters 配置

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,9 +51,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        ndk {
-            abiFilters.addAll(setOf("armeabi-v7a", "arm64-v8a", "x86_64"))
-        }
     }
 
     signingConfigs {
