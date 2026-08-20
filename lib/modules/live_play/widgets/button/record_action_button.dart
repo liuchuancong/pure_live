494035fe25d5dec0ea7a78f45a430ecdbb86c675
liuchuancong <17792321552@163.com>
2026-08-20 19:42:41 +0800

    feat: add CompactDanmakuOverlay widget for PiP danmaku display
    - Implemented CompactDanmakuOverlay to manage danmaku settings in Picture-in-Picture mode.
    - Integrated reactive state management using GetX for dynamic updates.
    - Configured danmaku appearance based on user settings, including font size, weight, opacity, and speed.
    - Ensured compatibility with emoji and stroke options for enhanced visual presentation.

--- lib/modules/live_play/widgets/button/record_action_button.dart
+++ lib/modules/live_play/widgets/button/record_action_button.dart
@@ -148,6 +148,7 @@ class RecordActionButton extends StatelessWidget {
         break;
 
       case "page":
+        // to 会崩溃 但是注释掉就可以 待解决
         Get.toNamed(RoutePath.kRecordPage);
         break;
     }
@@ -266,8 +267,7 @@ class RecordActionButton extends StatelessWidget {
                 color: theme.colorScheme.primary,
                 enabled: true,
                 onTap: () {
-                  // 貌似Video有问题无法进行页面跳转 否则会崩溃 但是注释掉就可以 待解决
-                  Get.toNamed(RoutePath.kRecordPage);
+                  Navigator.pop(dialogContext, "page");
                 },
               ),
               _ActionTile(
