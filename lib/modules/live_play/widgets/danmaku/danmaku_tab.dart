8c6df89df6d05dcc51a8106fcd12d605793d3083
bobobo <36957912+liuchuancong@users.noreply.github.com>
2026-08-29 20:46:18 +0800

    更新 danmaku_tab.dart

--- lib/modules/live_play/widgets/danmaku/danmaku_tab.dart
+++ lib/modules/live_play/widgets/danmaku/danmaku_tab.dart
@@ -65,7 +65,7 @@ class DanmakuSectionTabBar extends StatelessWidget {
       child: TabBar(
         key: const ValueKey('live-danmaku-section-tabs'),
         isScrollable: false,
-        tabAlignment: TabAlignment.center,
+        tabAlignment: TabAlignment.fill,
         physics: const PureLiveBoundedScrollPhysics(),
         labelPadding: const EdgeInsets.symmetric(horizontal: 4),
         controller: controller,
