3c7c8fc4830782bfc7e455f6775f35e15ef2d529
liuchuancong <chuancong@wangdou-hbean.com>
2026-09-01 10:32:25 +0800

    feat: 添加 messageId 字段到 LiveSuperChatMessage 类，并在 HuyaDanmaku 中使用

--- lib/core/site/huya/huya_site.dart
+++ lib/core/site/huya/huya_site.dart
@@ -1072,7 +1072,7 @@ class HuyaSite implements LiveSite, LiveSiteRoomRefresher, LiveSiteRecordRoomRes
       ..lPid = lPid
       ..tId = userId
       ..iMessageBoardScope = 0
-      ..iPageSize = 10;
+      ..iPageSize = 50;
 
     final rsp = await messageBoardClient.tupRequest("getHeadLineMessageBoard", req, GetGameEventMessageBoardRsp());
 
