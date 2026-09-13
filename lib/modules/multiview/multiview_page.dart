d54d8351114d2c805e4c6ec450c23efe7948864e
liuchuancong <17792321552@163.com>
2026-09-13 17:01:31 +0800

    fix(*)

--- lib/modules/multiview/multiview_page.dart
+++ lib/modules/multiview/multiview_page.dart
@@ -13,6 +13,7 @@ import 'package:pure_live/modules/live_play/controllers/player_state.dart';
 import 'package:pure_live/player/widgets/video_output_viewport_sizer.dart';
 import 'package:pure_live/modules/live_play/pages/danmaku_settings_page.dart';
 import 'package:pure_live/modules/multiview/widgets/multiview_room_picker.dart';
+import 'package:pure_live/modules/live_play/widgets/layout/live_play_back_scope.dart';
 import 'package:pure_live/modules/multiview/widgets/multiview_fullscreen_surface.dart';
 import 'package:pure_live/modules/multiview/danmaku/multiview_danmaku_settings_binding.dart';
 
@@ -106,8 +107,7 @@ class _MultiviewPageState extends State<MultiviewPage> {
   }
 
   /// 返回意图统一入口：非 normal 先回 normal，normal 走安全退出序列。
-  void _handleBackIntent({required bool didPop}) {
-    if (didPop) return;
+  void _handleBackIntent() {
     if (_displayMode != _DisplayMode.normal) {
       unawaited(_changeDisplayMode(_DisplayMode.normal));
       return;
@@ -329,9 +329,9 @@ class _MultiviewPageState extends State<MultiviewPage> {
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      onPopInvokedWithResult: (didPop, _) => _handleBackIntent(didPop: didPop),
+    return LivePlayBackScope(
+      presentationActive: _displayMode != _DisplayMode.normal,
+      onExitPresentation: _handleBackIntent,
       child: switch (_displayMode) {
         _DisplayMode.normal => Scaffold(
           appBar: AppBar(
