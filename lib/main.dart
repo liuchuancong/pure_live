1a0c3243ab391cf5bd08413a5bd6a015c2fc2101
liuchuancong <17792321552@163.com>
2026-09-13 14:37:31 +0800

    feat(theme): implement dynamic theme application logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'dart:io';
 import 'dart:async';
-
 import 'package:flutter/foundation.dart';
 import 'package:pure_live/common/index.dart';
-import 'package:pure_live/common/services/settings/player_settings_controller.dart';
+import 'package:pure_live/plugins/file_utils.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:pure_live/common/global/initialized.dart';
+import 'package:material_ui/material_ui.dart' as material;
 import 'package:pure_live/player/utils/player_consts.dart';
 import 'package:pure_live/routes/navigation_observer.dart';
 import 'package:pure_live/player/models/player_engine.dart';
 import 'package:pure_live/common/global/platform_utils.dart';
 import 'package:pure_live/routes/route_observer_controller.dart';
+import 'package:pure_live/common/utils/shared_media_intake.dart';
+import 'package:pure_live/common/utils/share_command_handler.dart';
 import 'package:pure_live/core/iptv/services/epg_import_manager.dart';
 import 'package:pure_live/common/global/platform/desktop_manager.dart';
-import 'package:pure_live/common/utils/share_command_handler.dart';
-import 'package:pure_live/common/utils/shared_media_intake.dart';
 import 'package:pure_live/core/iptv/services/iptv_import_manager.dart';
-import 'package:pure_live/plugins/file_utils.dart';
-import 'package:material_ui/material_ui.dart' as material;
+import 'package:pure_live/common/services/settings/player_settings_controller.dart';
+
 
 void main(List<String> args) async {
   // Flutter abbreviates every framework error after the first one. In release
@@ -50,6 +50,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> with DesktopWindowMixin {
   SharedMediaReceiver? _sharedMediaReceiver;
+  bool _dynamicThemeChangeScheduled = false;
 
   @override
   void initState() {
@@ -126,6 +127,41 @@ class _MyAppState extends State<MyApp> with DesktopWindowMixin {
     await receiver.start();
   }
 
+  void _applyDynamicTheme(
+    material.ColorScheme? lightDynamic,
+    material.ColorScheme? darkDynamic,
+    ThemeData lightThemeData,
+    ThemeData darkThemeData,
+  ) {
+    if (_dynamicThemeChangeScheduled) {
+      return;
+    }
+
+    _dynamicThemeChangeScheduled = true;
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _dynamicThemeChangeScheduled = false;
+
+      if (!mounted) {
+        return;
+      }
+
+      final brightness = Theme.of(context).brightness;
+
+      if (SettingsService.to.theme.enableDynamicTheme.v && lightDynamic != null && darkDynamic != null) {
+        final scheme = brightness == Brightness.dark
+            ? toFlutterColorScheme(darkDynamic)
+            : toFlutterColorScheme(lightDynamic);
+
+        final theme = MyTheme(colorScheme: scheme);
+
+        Get.changeTheme(brightness == Brightness.dark ? theme.darkThemeData : theme.lightThemeData);
+      } else {
+        Get.changeTheme(brightness == Brightness.dark ? darkThemeData : lightThemeData);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
@@ -145,6 +181,7 @@ class _MyAppState extends State<MyApp> with DesktopWindowMixin {
             lightTheme = MyTheme(primaryColor: themeColor).lightThemeData;
             darkTheme = MyTheme(primaryColor: themeColor).darkThemeData;
           }
+          _applyDynamicTheme(lightDynamic, darkDynamic, lightTheme, darkTheme);
 
           return GetMaterialApp(
             // The localized title is rendered by CustomTitleBar. A stable
