135aa3185ac29546ca87702b560e28a548a0a623
wzgrx <39661556+wzgrx@users.noreply.github.com>
2026-09-20 05:37:41 +0800

    refactor: remove superseded legacy libraries

--- lib/modules/live_play/widgets/index.dart
+++
@@ -1,6 +0,0 @@
-library;
-
-export './video_player/video_controller.dart';
-export './video_player/video_player.dart';
-export 'danmaku/danmaku_list_view.dart';
-export '../dialogs/live_dlna_dialog.dart';
