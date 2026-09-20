3b5a32343a41fbe6ca8703c875f1921afe2e2a21
wzgrx <39661556+wzgrx@users.noreply.github.com>
2026-09-20 15:12:05 +0800

    fix: enlarge favorite areas action

--- lib/modules/areas/areas_page.dart
+++ lib/modules/areas/areas_page.dart
@@ -55,21 +55,24 @@ class AreasPage extends GetView<AreasController> {
                   child: InkWell(
                     borderRadius: BorderRadius.circular(16),
                     onTap: () => Get.toNamed(RoutePath.kFavoriteAreas),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(Remix.heart_add_2_line, size: 16, color: Theme.of(context).colorScheme.primary),
-                          const SizedBox(width: 8),
-                          Text(
-                            i18n("favorite_areas"),
-                            style: AppTextStyles.t12Bold.copyWith(
-                              color: Theme.of(context).colorScheme.primary,
-                              letterSpacing: 0.5,
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints(minHeight: kMinInteractiveDimension),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Remix.heart_add_2_line, size: 16, color: Theme.of(context).colorScheme.primary),
+                            const SizedBox(width: 8),
+                            Text(
+                              i18n("favorite_areas"),
+                              style: AppTextStyles.t12Bold.copyWith(
+                                color: Theme.of(context).colorScheme.primary,
+                                letterSpacing: 0.5,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
