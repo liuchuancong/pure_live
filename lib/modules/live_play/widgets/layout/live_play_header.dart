41c08f392b7bc3246f530afdcf81ae559e1f27eb
liuchuancong <chuancong@wangdou-hbean.com>
2026-09-23 14:02:41 +0800

    删除未使用的缓存网络图片导入

--- lib/modules/live_play/widgets/layout/live_play_header.dart
+++ lib/modules/live_play/widgets/layout/live_play_header.dart
@@ -1,6 +1,5 @@
 import 'package:pure_live/common/index.dart';
 import 'package:pure_live/common/widgets/common_avatar.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:pure_live/modules/live_play/controllers/live_play_controller.dart';
 import 'package:pure_live/modules/live_play/widgets/button/record_action_button.dart';
 import 'package:pure_live/modules/live_play/widgets/button/live_play_menu_button.dart';
